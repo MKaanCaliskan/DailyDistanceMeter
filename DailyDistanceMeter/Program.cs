56b8deed08ce8167f1ed365aed7647413328158e
MUSTAFA KAAN ÇALIŞKAN <mkaancaliskan@gmail.com>
2022-04-02 12:49:50 +0300

    Add project files.

---
+++ DailyDistanceMeter/Program.cs
@@ -0,0 +1,219 @@
+﻿using System;
+
+namespace BilgeAdam.DailyRunningMeter
+{
+    public class Program
+    {
+
+
+        public static void Main(string[] args)
+        {
+            var flag = true;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Günlük Koşu Mesafesi Ölçer ");
+                var a = StepSize();
+                var b = StepNumberInMinute();
+                var c = RunningTime();
+                var result = CalculateDistance(a, b, c);
+                Console.WriteLine("");
+                Console.WriteLine("--------------------------------------------------------------------");
+                Console.WriteLine($"Koşu mesafeniz : {result} metre ");
+                Console.WriteLine("--------------------------------------------------------------------");
+                Console.WriteLine("");
+                var yesOrNo = true;
+                do
+                {
+                    Console.WriteLine("Programa devam etmek ister misiniz? EVET yada HAYIR yazınız :");
+                    var answer = Console.ReadLine().ToUpper();
+                    if (answer == "EVET" || answer == "E")
+                    {
+
+                        yesOrNo = false;
+                    }
+                    else if (answer == "HAYIR" || answer == "H")
+                    {
+                        yesOrNo = false;
+                        flag = false;
+                        Console.WriteLine("Programı kullandığınız için teşşekkürler.");
+
+                    }
+                    else
+                    {
+
+                        Console.WriteLine("************************");
+                        Console.WriteLine("Hatalı giriş yaptınız!");
+                        Console.WriteLine("************************");
+                        yesOrNo = true;
+                    }
+
+                } while (yesOrNo);
+
+
+            } while (flag);
+
+
+        }
+
+        static int StepSize()
+        {
+            var stepDistance = 0;
+            int sayi;
+            var flag1 = true;
+            do
+            {
+                Console.WriteLine("--------------------------------------------------------------------");
+                Console.WriteLine("Ortalama bir adımınızın Kaç cm olduğunu giriniz(cm):");
+                var step = Console.ReadLine();
+
+                if (!int.TryParse(step, out sayi))
+                {
+                    Console.WriteLine("Hatalı giriş yaptınız!");
+                }
+                else
+                {
+                    stepDistance = Convert.ToInt32(step);
+                    if (stepDistance <= 0)
+                    {
+                        Console.WriteLine("************************");
+                        Console.WriteLine("Negatif ve sıfır  değeri giremezsiniz !");
+                        Console.WriteLine("************************");
+                    }
+                    else
+                    {
+                        flag1 = false;
+                    }
+
+                }
+
+            } while (flag1);
+
+            return stepDistance;
+        }
+        static int StepNumberInMinute()
+        {
+            var stepNumber = 0;
+            int sayi1;
+            var flag2 = true;
+            do
+            {
+                Console.WriteLine("--------------------------------------------------------------------");
+                Console.WriteLine("Bir dakikada kaç adım attığınızı giriniz:");
+                var stepMinute = Console.ReadLine();
+
+                if (!int.TryParse(stepMinute, out sayi1))
+                {
+                    Console.WriteLine("************************");
+                    Console.WriteLine("Hatalı giriş yaptınız!");
+                    Console.WriteLine("************************");
+                }
+                else
+                {
+                    stepNumber = Convert.ToInt32(stepMinute);
+                    if (stepNumber <= 0)
+                    {
+                        Console.WriteLine("************************");
+                        Console.WriteLine("Negatif ve sıfır  değeri giremezsiniz !");
+                        Console.WriteLine("************************");
+                    }
+                    else
+                    {
+                        flag2 = false;
+                    }
+
+                }
+
+
+            } while (flag2);
+
+            return stepNumber;
+        }
+        static int RunningTime()
+        {
+            var durationHour = 0;
+            var durationMinute = 0;
+            int sayi2;
+            var flag3 = true;
+            var flag4 = true;
+            Console.WriteLine("--------------------------------------------------------------------");
+            Console.WriteLine("Koşu süresini önce saat sonra olarak giriniz:");
+            do
+            {
+                Console.Write("Saat :");
+                var durationTimeHour = Console.ReadLine();
+                if (!int.TryParse(durationTimeHour, out sayi2))
+                {
+                    Console.WriteLine("************************");
+                    Console.WriteLine("Hatalı giriş yaptınız!");
+                    Console.WriteLine("************************");
+                }
+                else
+                {
+
+                    durationHour = Convert.ToInt32(durationTimeHour);
+
+                    if (durationHour < 0)
+                    {
+                        Console.WriteLine("************************");
+                        Console.WriteLine("Negatif değer giremezsiniz !");
+                        Console.WriteLine("************************");
+                    }
+                    else
+                    {
+                        flag3 = false;
+                    }
+                }
+
+            } while (flag3);
+
+            do
+            {
+
+                Console.Write("Dakika:");
+                var durationTimeMinute = Console.ReadLine();
+                if (!int.TryParse(durationTimeMinute, out sayi2))
+                {
+                    Console.WriteLine("************************");
+                    Console.WriteLine("Hatalı giriş yaptınız!");
+                    Console.WriteLine("************************");
+                }
+                else
+                {
+                    durationMinute = Convert.ToInt32(durationTimeMinute);
+                    if (durationMinute > 60)
+                    {
+                        Console.WriteLine("************************");
+                        Console.WriteLine("60 sayısından büyük dakika giremezsiniz !");
+                        Console.WriteLine("************************");
+                    }
+                    else if (durationMinute <= 0)
+                    {
+                        Console.WriteLine("************************");
+                        Console.WriteLine("0 ve daha küçük dakika giremezsiniz !");
+                        Console.WriteLine("************************");
+                    }
+                    else
+                    {
+                        flag4 = false;
+                    }
+
+                }
+
+            } while (flag4);
+
+            var total = (durationHour * 60) + durationMinute;
+            return total;
+
+        }
+        public static double CalculateDistance(int a, int b, int c)
+        {
+            var runningtime = Convert.ToDouble((a * b * c) / 100);
+
+            return runningtime;
+        }
+
+    }
+}
+
---
+++ TestProjectDailyDistanceMeter/CalculateRunningDistance.cs
@@ -0,0 +1,24 @@
+﻿using BilgeAdam.DailyRunningMeter;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectKosuOlcer
+{
+    [TestFixture]
+    internal class CalculateRunningDistance
+    {
+        [TestCase(5, 30, 90, 135)]
+        [TestCase(40, 20, 195, 1560)]
+        [TestCase(20, 47, 70, 658)]
+
+        public void Calculate_distance_tree_variable(int stepSizeCm, int stepNumber, int timeTotalMinute, double expected)
+        {
+            var sample = Program.CalculateDistance(stepSizeCm, stepNumber, timeTotalMinute);
+            Assert.AreEqual(sample, expected);
+        }
+    }
+}
